9102c41f163723b67e6189db027900c5ba3b388c
W3tStrang <jkehoe997@gmail.com>
2023-10-05 22:18:32 -0400

    created AuthKeyInput module

---
+++ src/lib/AuthKeyInput.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { invoke } from "@tauri-apps/api/tauri";
+
+    let hasAuth: boolean;
+    invoke("has_authkey")
+        .then((result: boolean) => hasAuth = result)
+        .catch(() => hasAuth = false);
+        
+</script>
