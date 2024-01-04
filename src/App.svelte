ead58143a086ea6e17a46cd0c47708e91ac99602
W3tStrang <jkehoe997@gmail.com>
2024-01-03 23:24:37 -0500

    started implementing bottom bar

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,11 @@
   import ServerStatusBar from "./lib/ServerStatusBar.svelte";
   import MapList from "./lib/MapList.svelte";
   import ModList from "./lib/ModList.svelte";
+  import BottomBar from "./lib/BottomBar.svelte";
 </script>
 
 <main class="container">
+  <!-- <div data-tauri-drag-region class="titlebar"></div> -->
   <div class="server-status-bar">
     <ServerStatusBar />
   </div>
@@ -19,15 +21,15 @@
       </div>
     </div>
   </div>
+  <div class="bottom-bar">
+    <BottomBar />
+  </div>
 </main>
 
 <style>
-  .main-content {
-    display: flex;
-  }
-
   .content-lists {
     display: flex;
+    justify-content: center;
     padding: 15px;
   }
 </style>
