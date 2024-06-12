2f8a0a1eca6f4875e613e1d3123abd24637a9513
W3tStrang <jkehoe997@gmail.com>
2024-06-12 00:27:01 -0400

    implemented map selection

--- src/lib/MapList.svelte
+++ src/lib/MapList.svelte
@@ -2,6 +2,9 @@
     import { invoke } from "@tauri-apps/api/tauri";
     import type { Mod } from "./mod";
     import MapListItem from "./MapListItem.svelte";
+    import { onMount } from "svelte";
+    import { current_map } from "./stores";
+    import VMapListItem from "./VMapListItem.svelte";
 
     let maps = [
         "Gridmap v2",
@@ -28,13 +31,33 @@
             .catch((_) => mod_maps = []);
     }
 
-    getModMaps();
+    async function getCurrentMap() {
+        await invoke("get_current_map")
+            .then((_current_map: string) => current_map.set(_current_map))
+            .catch((_) => current_map.set("gridmap_v2"));
+    }
+
+    onMount(() => {
+        getModMaps();
+        getCurrentMap();
+    })
+
+    // getModMaps();
 </script>
 
 <div>
     <ul>
+        <!--Vanilla Maps-->
+        <svelte:component this={VMapListItem} internal_name={"gridmap_v2"} external_name={"Gridmap v2"}/>
+        <!--Modded Maps-->
         {#each mod_maps as item}
-            <li><svelte:component this={MapListItem} modObject={item}/></li>
+            <svelte:component this={MapListItem} modObject={item}/>
         {/each}
     </ul>
 </div>
+
+<style>
+    ul {
+        list-style-type: none;
+    }
+</style>
--- src/lib/MapListItem.svelte
+++ src/lib/MapListItem.svelte
@@ -1,10 +1,80 @@
 <script lang="ts">
+    import { invoke } from "@tauri-apps/api/tauri";
     import type { Mod } from "./mod";
+    import { current_map } from "./stores";
 
     export let modObject: Mod;
+
+    let is_active = false;
+
+    async function setAsCurrentMap() {
+        await invoke("change_map", { mapName: modObject.internal_name });
+        current_map.set(modObject.internal_name);
+    }
+
+    current_map.subscribe((map) => {
+        if (map == modObject.internal_name) {
+            is_active = true;
+        } else {
+            is_active = false;
+        }
+    });
+
+    async function deleteMap() {
+        // TODO: implement
+    }
 
 </script>
 
 <li>
-    {modObject.internal_name}
+    <div class="main-body" on:dblclick={setAsCurrentMap} class:active={is_active}>
+        <div class="details">
+            <p class="map-name">{modObject.details["title"]}</p>
+            <p class="internal-name">{modObject.internal_name}</p>
+        </div>
+        <div class="action-button">
+            <button class="delete button" on:click={deleteMap}>
+                <svg
+                    style="fill: currentColor;"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512">
+                        <path
+                            d="M199 103v50h-78v30h270v-30h-78v-50H199zm18 18h78v32h-78v-32zm-79.002 80l30.106 286h175.794l30.104-286H137.998zm62.338 13.38l.64 8.98 16 224 .643 8.976-17.956 1.283-.64-8.98-16-224-.643-8.976 17.956-1.283zm111.328 0l17.955 1.284-.643 8.977-16 224-.64 8.98-17.956-1.284.643-8.977 16-224 .64-8.98zM247 215h18v242h-18V215z"
+                        />
+                </svg>
+                <!-- <img src="/trash-can.svg" alt="Delete" /> -->
+            </button>
+        </div>
+
+    </div>
 </li>
+
+<style>
+    .main-body {
+        background-color: #2b2b2b;
+        height: 6rem;
+        width: 20rem;
+        border-style: none;
+        border-radius: 8px;
+        display: flex;
+    }
+    .main-body:hover {
+        background-color: #313131;
+    }
+    .active {
+        border-style: solid !important;
+        border-width: 3px;
+        border-color: #ff7722;
+    }
+    .details {
+        justify-self: center;
+    }
+    .delete.button svg {
+        color: inherit;
+        height: 75%;
+        
+    }
+    .delete.button:hover, .delete.button svg:hover {
+        color: rgb(252, 77, 77);
+    }
+</style>
