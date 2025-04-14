a721aed0aa9f966438077e8a9e89dc90e142434c
W3tStrang <jkehoe997@gmail.com>
2025-04-14 12:51:11 -0400

    removed old unused files

--- src/lib/AuthKeyInput.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import { invoke } from "@tauri-apps/api/tauri";
-
-    let hasAuth: boolean;
-    invoke("has_authkey")
-        .then((result: boolean) => hasAuth = result)
-        .catch(() => hasAuth = false);
-        
-</script>
