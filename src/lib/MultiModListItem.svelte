866b28083b92ef00c06a99a27cbceac39557c735
Finn-Kehoe <jkehoe997@gmail.com>
2026-06-07 14:54:18 -0400

    standardized css formatting

--- src/lib/MultiModListItem.svelte
+++ src/lib/MultiModListItem.svelte
@@ -161,54 +161,54 @@
         color: rgb(252, 77, 77);
     }
     .switch {
-    position: relative;
-    display: inline-block;
-    width: 50px;
-    height: 24px;
+        position: relative;
+        display: inline-block;
+        width: 50px;
+        height: 24px;
     }
     .switch input {
-    opacity: 0;
-    width: 0;
-    height: 0;
+        opacity: 0;
+        width: 0;
+        height: 0;
     }
     .slider {
-    position: absolute;
-    cursor: pointer;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: #1a1a1a;
-    -webkit-transition: .4s;
-    transition: .4s;
+        position: absolute;
+        cursor: pointer;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: #1a1a1a;
+        -webkit-transition: .4s;
+        transition: .4s;
     }
     .slider:before {
-    position: absolute;
-    content: "";
-    height: 20px;
-    width: 20px;
-    left: 2px;
-    bottom: 2px;
-    background-color: white;
-    -webkit-transition: .4s;
-    transition: .4s;
+        position: absolute;
+        content: "";
+        height: 20px;
+        width: 20px;
+        left: 2px;
+        bottom: 2px;
+        background-color: white;
+        -webkit-transition: .4s;
+        transition: .4s;
     }
     input:checked + .slider {
-    background-color: #ff7722;
+        background-color: #ff7722;
     }
     input:focus + .slider {
-    box-shadow: 0 0 1px #ff7722;
+        box-shadow: 0 0 1px #ff7722;
     }
     input:checked + .slider:before {
-    -webkit-transform: translateX(26px);
-    -ms-transform: translateX(26px);
-    transform: translateX(26px);
+        -webkit-transform: translateX(26px);
+        -ms-transform: translateX(26px);
+        transform: translateX(26px);
     }
     .slider.round {
-    border-radius: 34px;
+        border-radius: 34px;
     }
     .slider.round:before {
-    border-radius: 50%;
+        border-radius: 50%;
     }
     .expanded-body {
         margin-left: auto;
